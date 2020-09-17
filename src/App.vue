a83e22d8c2b40e2ccb7b79b396fb85d1a3fb4588
Carolina-OH <carito.92@gmail.com>
2020-09-17 14:45:56 -0300

    intentando hacer el desafio, aun falta la comunicacion entre componenttre componentes padres e hijos por props y emit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app" class="text-center">
+    <span id="titulo" class="align-bottom mr-4">Lista de tareas</span>
+        <img src="src/img/lista-de-verificacion.svg">
+    <div id="tarea" class="col-3-4 d-flex flex-column align-items-center mt-2">
+        <h4 class="mt-2">{{message}}</h4>
+        <input type="text" v-model="skill" placeholder="ingrese una nueva tarea" class="mt-2 mb-3 text-center" @keyup.enter="add_list">
+        <button v-on:click=add_list class="mt-2 mb-5 btn btn-secondary btn-sm"> Crear </button>
+        <h5><u>Lista</u></h5>
+        <ul class="px-0">
+            <li v-for="skill in skills" :key="skill.id">{{skill}}<eliminar v-bind:key="skill.id" id="skill.id"></eliminar></li>
+        </ul>
+    </div>
+  </div>
+</template>
+<script>
+import eliminar from "./eliminar.vue"
+  export default {
+    props:{
+        id:Number,
+        require: true,
+    },
+    data() {
+      return {
+
+        message: "Crea una nueva tarea",
+        skills:[],
+        skill:"",
+    };
+    },
+    components:{eliminar},
+    methods:{
+        add_list: function () { 
+            if(this.skill != ""){
+            this.skills.push(this.skill)
+            this.skill="";
+            }
+        }
+    }
+  }
+</script>
+
+<style scoped>
+
+#titulo{
+    color: indigo;
+    font-size: 70px;
+}
+h4{
+    color: blueviolet;
+}
+
+input{
+    color:gray;
+}
+img{
+    width: 5%;
+    align-content: flex-start;
+}
+
+</style>
---
+++ src/eliminar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <a href="#" @click.stop="eliminar(id)"><i class="fas fa-trash-alt"></i></a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "eliminar",
+    data() {
+      return {
+        message: ""
+      };
+    },
+    methods:{
+        eliminar(id){
+            alert(id)
+        }
+    },
+    mounted:function(){
+       // this.count()
+    }
+  }
+
+</script>
+
+<style scoped>
+#app{
+    display: inline-block;
+    margin-left:20px;
+}
+a:hover{
+    color: black;
+}
+a{
+    color:grey
+}
+i{
+    font-size:15px;
+}
+</style>
